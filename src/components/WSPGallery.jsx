2a0c8129f4c72c129991188ef95d5df7fc3584f7
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2024-04-27 19:04:45 +0300

    commit

---
+++ src/components/WSPGallery.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import "./WSPGallery.css";
+
+const WSPGallery = ({ galleryImages }) => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImages.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+  const nextSlide = () => {
+    slideNumber === galleryImages.length - 1
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
+  return (
+    <div>
+      {openModal && (
+        <div className="sliderWrap">
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className="btnClose"
+            onClick={handleCloseModal}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronLeft}
+            className="btnPrev"
+            onClick={prevSlide}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            className="btnNext"
+            onClick={nextSlide}
+          />
+          <div className="fullScreenImage">
+            <img src={galleryImages[slideNumber].img} alt="" />
+          </div>
+        </div>
+      )}
+      <p>Currrent slide number : {slideNumber + 1}</p>
+      <p>Total slides : {galleryImages.length}</p>
+      <div className="galleryWrap">
+        {galleryImages &&
+          galleryImages.map((slide, index) => {
+            return (
+              <div
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
+                <img src={slide.img} alt="" />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default WSPGallery;
